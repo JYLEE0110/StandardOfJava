f4d50c51bddaf7346343b9cb44c6da5bb5d7cfc8
JY LEE <'wndyd0110@gmail.com'>
2023-10-18 20:39:31 +0900

    Collection 프레임워크 연습문제

---
+++ standardOfJava/src/ch_11/Exercise11_3.java
@@ -0,0 +1,78 @@
+package ch_11;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+class Student implements Comparable{
+	
+	private String name;
+	private int ban;
+	private int no;
+	private int kor, eng, math;
+	
+	public Student(String name, int ban, int no, int kor, int eng, int math) {
+		this.name = name;
+		this.ban = ban;
+		this.no = no;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+	
+	int getTotal() {
+		return kor + eng + math;
+	}
+	
+	float getAverage() {
+		return (int)((getTotal() / 3f) * 10 + 0.5) / 10f;
+	}
+
+	@Override
+	public String toString() {
+		return name + "," + ban + "," + no + "," + kor + "," + eng + "," + math + "," +getTotal() + "," + getAverage();
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		
+		int result;
+		
+		if(o instanceof Student) {
+			
+			Student name = (Student)o;
+			
+			return this.name.compareTo(name.name);
+
+		}else {
+			return -1;
+		}
+		
+	}
+	
+	
+	
+}
+
+public class Exercise11_3 {
+	
+	public static void main(String[] args) {
+		
+		ArrayList list = new ArrayList();
+		list.add(new Student("홍길동", 1, 1, 100, 100, 100));
+		list.add(new Student("남궁성", 1, 1, 100, 100, 100));
+		list.add(new Student("김자바", 1, 1, 100, 100, 100));
+		list.add(new Student("이자바", 1, 1, 100, 100, 100));
+		list.add(new Student("안자바", 1, 1, 100, 100, 100));
+		
+		Collections.sort(list);
+		
+		Iterator it = list.iterator();
+		
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+
+	}
+
+}
---
+++ standardOfJava/src/ch_11/Exercise11_5.java
@@ -0,0 +1,79 @@
+package ch_11;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+
+class Student2{
+	
+	private String name;
+	private int ban;
+	private int no;
+	private int kor, eng, math;
+	
+	public Student2(String name, int ban, int no, int kor, int eng, int math) {
+		this.name = name;
+		this.ban = ban;
+		this.no = no;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+	
+	int getTotal() {
+		return kor + eng + math;
+	}
+	
+	float getAverage() {
+		return (int)((getTotal() / 3f) * 10 + 0.5) / 10f;
+	}
+
+	@Override
+	public String toString() {
+		return name + "," + ban + "," + no + "," + kor + "," + eng + "," + math + "," +getTotal() + "," + getAverage();
+	}
+}
+
+class BanNoAscending implements Comparator{
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		
+		if(o1 instanceof Student2 && o2 instanceof Student2) {
+			
+			Student2 s1 = (Student2)o1;
+			Student2 s2 = (Student2)o2;
+			// comparTo로 비교
+			
+		}
+		
+		return 0;
+	}
+	
+	
+	
+}
+
+public class Exercise11_5 {
+	
+	public static void main(String[] args) {
+		
+		ArrayList list = new ArrayList();
+		list.add(new Student("홍길동", 1, 3, 100, 100, 100));
+		list.add(new Student("남궁성", 1, 1, 100, 100, 100));
+		list.add(new Student("김자바", 1, 2, 100, 100, 100));
+		list.add(new Student("이자바", 2, 1, 100, 100, 100));
+		list.add(new Student("안자바", 2, 2, 100, 100, 100));
+		
+		Collections.sort(list, new BanNoAscending());
+		
+		Iterator it = list.iterator();
+		
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+
+	}
+
+}
