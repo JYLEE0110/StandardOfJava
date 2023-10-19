363a8f3d841cabe0ad3da901da0603ebcf5f514a
JY LEE <'wndyd0110@gmail.com'>
2023-10-19 18:03:46 +0900

    hashCode 사용

--- standardOfJava/src/ch_11/Exercise11_5.java
+++ standardOfJava/src/ch_11/Exercise11_5.java
@@ -7,10 +7,10 @@ import java.util.Iterator;
 
 class Student2{
 	
-	private String name;
-	private int ban;
-	private int no;
-	private int kor, eng, math;
+	String name;
+	int ban;
+	int no;
+	int kor, eng, math;
 	
 	public Student2(String name, int ban, int no, int kor, int eng, int math) {
 		this.name = name;
@@ -42,13 +42,17 @@ class BanNoAscending implements Comparator{
 		
 		if(o1 instanceof Student2 && o2 instanceof Student2) {
 			
+			
 			Student2 s1 = (Student2)o1;
 			Student2 s2 = (Student2)o2;
-			// comparTo로 비교
 			
+			// comparTo로 비교
+			return s1.ban == s2.ban ? (s1.no - s2.no) : s1.ban-s2.ban;
+			
+		}else {
+			return -1;
 		}
 		
-		return 0;
 	}
 	
 	
@@ -60,11 +64,11 @@ public class Exercise11_5 {
 	public static void main(String[] args) {
 		
 		ArrayList list = new ArrayList();
-		list.add(new Student("홍길동", 1, 3, 100, 100, 100));
-		list.add(new Student("남궁성", 1, 1, 100, 100, 100));
-		list.add(new Student("김자바", 1, 2, 100, 100, 100));
-		list.add(new Student("이자바", 2, 1, 100, 100, 100));
-		list.add(new Student("안자바", 2, 2, 100, 100, 100));
+		list.add(new Student2("홍길동", 1, 3, 100, 100, 100));
+		list.add(new Student2("남궁성", 1, 1, 100, 100, 100));
+		list.add(new Student2("김자바", 1, 2, 100, 100, 100));
+		list.add(new Student2("이자바", 2, 1, 100, 100, 100));
+		list.add(new Student2("안자바", 2, 2, 100, 100, 100));
 		
 		Collections.sort(list, new BanNoAscending());
 		
